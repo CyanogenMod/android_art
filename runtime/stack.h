811fcb11248912cf4768e7974a2e906c5f134262
Geetesh Kalakoti <kalakotig@gmail.com>
2014-07-21 19:18:14 -0500

    4-byte align 64-bit values in Get/Set Double/Long for GCC 4.8.
    This will allow people to build with Google GCC 4.8.
    
    Original Committer: Jeff Hao
    Signed-off-by: Geetesh Kalakoti <kalakotig@gmail.com>

--- runtime/stack.h
+++ runtime/stack.h
@@ -138,13 +138,15 @@ class ShadowFrame {
   int64_t GetVRegLong(size_t i) const {
     DCHECK_LT(i, NumberOfVRegs());
     const uint32_t* vreg = &vregs_[i];
-    return *reinterpret_cast<const int64_t*>(vreg);
+    typedef const int64_t unaligned_int64 __attribute__ ((aligned (4)));
+    return *reinterpret_cast<unaligned_int64*>(vreg);
   }
 
   double GetVRegDouble(size_t i) const {
     DCHECK_LT(i, NumberOfVRegs());
     const uint32_t* vreg = &vregs_[i];
-    return *reinterpret_cast<const double*>(vreg);
+    typedef const double unaligned_double __attribute__ ((aligned (4)));
+    return *reinterpret_cast<unaligned_double*>(vreg)
   }
 
   mirror::Object* GetVRegReference(size_t i) const {
@@ -177,7 +179,8 @@ class ShadowFrame {
   void SetVRegLong(size_t i, int64_t val) {
     DCHECK_LT(i, NumberOfVRegs());
     uint32_t* vreg = &vregs_[i];
-    *reinterpret_cast<int64_t*>(vreg) = val;
+    typedef int64_t unaligned_int64 __attribute__ ((aligned (4)));
+    *reinterpret_cast<unaligned_int64*>(vreg) = val;
   }
 
   void SetVRegDouble(size_t i, double val) {
@@ -189,7 +192,8 @@ class ShadowFrame {
   void SetVRegReference(size_t i, mirror::Object* val) {
     DCHECK_LT(i, NumberOfVRegs());
     uint32_t* vreg = &vregs_[i];
-    *reinterpret_cast<mirror::Object**>(vreg) = val;
+    typedef double unaligned_double __attribute__ ((aligned (4)));
+    *reinterpret_cast<unaligned_double*>(vreg) = val;
     if (HasReferenceArray()) {
       References()[i] = val;
     }
