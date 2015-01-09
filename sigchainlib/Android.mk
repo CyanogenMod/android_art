4880c1b2985e9327ce85ea3b251fc835ab81a5c4
Brian Chu <chub@nextbit.com>
2015-01-09 13:43:28 -0800

    libsigchain: Fix OSX compilation
    Change-Id: I8555280cbcee85c44af098cb9c804c958709125d

--- sigchainlib/Android.mk
+++ sigchainlib/Android.mk
@@ -53,6 +53,9 @@ LOCAL_SRC_FILES := sigchain_dummy.cc
 LOCAL_MODULE:= libsigchain
 LOCAL_ADDITIONAL_DEPENDENCIES += $(LOCAL_PATH)/Android.mk
 LOCAL_LDLIBS = -ldl
+ifeq ($(strip $(HOST_OS)),darwin)
+LOCAL_LDFLAGS += -Wl,-lstdc++
+endif
 LOCAL_MULTILIB := both
 include $(BUILD_HOST_SHARED_LIBRARY)
 
