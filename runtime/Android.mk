6ae5ca85c16e3a8ab91d609c9d32b278dc6a28c9
Stephen Hines <srhines@google.com>
2016-03-02 18:26:29 -0800

    Enable clang for ARM builds.
    Bug: http://b/25130937
    
    We disable the integrated assembler for the ARM interpreter because
    Clang can't handle the syntax present.
    
    Change-Id: I978d3f78e25819bb76ccb0f7f4b91a88e4b5a2de

--- runtime/Android.mk
+++ runtime/Android.mk
@@ -440,6 +440,7 @@ $$(ENUM_OPERATOR_OUT_GEN): $$(GENERATED_SRC_DIR)/%_operator_out.cc : $(LOCAL_PAT
   ifeq ($$(art_target_or_host),target)
     $$(eval $$(call set-target-local-clang-vars))
     $$(eval $$(call set-target-local-cflags-vars,$(2)))
+    LOCAL_CLANG_ASFLAGS_arm += -no-integrated-as
     LOCAL_CFLAGS_$(DEX2OAT_TARGET_ARCH) += -DART_DEFAULT_INSTRUCTION_SET_FEATURES="$(LIBART_TARGET_DEFAULT_INSTRUCTION_SET_FEATURES)"
     LOCAL_CFLAGS_$(2ND_DEX2OAT_TARGET_ARCH) += -DART_DEFAULT_INSTRUCTION_SET_FEATURES="$(2ND_LIBART_TARGET_DEFAULT_INSTRUCTION_SET_FEATURES)"
   else # host
